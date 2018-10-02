b5b0688f8a7c007e5c6a2a591b24960a690c6e9d
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2018-10-02 15:50:23 +1300

    Initial commit

---
+++ sample/app.py
@@ -0,0 +1,14 @@
+
+class App(object):
+
+    constant = 100
+
+    @classmethod
+    def sum(cls, x, y):
+        return x + y
+
+    def dont_cover(self, a):
+        return self.constant + a
+
+    def __str__(self):
+        return "App"
---
+++ sample/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+from sample.app import App
+
+
+class TestCase(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(3, App.sum(1, 2))
+
