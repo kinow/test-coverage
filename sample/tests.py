215b3c64e4d90ec031c1b7966c2a8e4219810aee
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-10-04 13:28:24 +1300

    Update tests.py
    Let's lower the coverage

--- sample/tests.py
+++ sample/tests.py
@@ -6,5 +6,6 @@ from sample.app import App
 class TestCase(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(3, App.sum(1, 2))
+        #self.assertEqual(3, App.sum(1, 2))
+        self.assertEqual(3, 3)
 
