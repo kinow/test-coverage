97a502d09311a514e0270f4bf9b71cf20b7fdb22
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-10-17 14:00:19 +1300

    Update tests.py

--- sample/tests.py
+++ sample/tests.py
@@ -4,7 +4,7 @@ from sample.app import App
 
 
 class TestCase(unittest.TestCase):
-
-    def test_sum(self):
-        self.assertEqual(3, App.sum(1, 2))
+    pass
+#    def test_sum(self):
+#        self.assertEqual(3, App.sum(1, 2))
 
